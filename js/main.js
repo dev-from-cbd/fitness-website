eb51e3d1b0bafd8fb5e38e65faf37652204d0a32
dev-from-cbd <veryfatwombat@gmail.com>
2023-02-05 08:35:16 +1030

    html+css+js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ menu.onclick = () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+let swiper = new Swiper(".home-slider", {
+  spaceBetween: 20,
+  effect: "fade",
+  loop: true,
+  centeredSlide: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
